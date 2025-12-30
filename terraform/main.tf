df47e4f162d2c844ad9caf687c12e7fb6fd11224
NaomiVK <naomi.hanffcra@gmail.com>
2025-12-30 08:25:17 -0500

    Add S3 backend for Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "cra-scam-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "cra-scam-terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
